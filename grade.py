8e8c323d28499639291b5a24bbcbeb3ebc611b86
palak583 <palakn968@gmail.com>
2026-08-04 18:19:33 +0530

    Add files via upload

---
+++ grade.py
@@ -0,0 +1,20 @@
+print(input("Enter student name:"))
+num1 = float(input("Enter Maths marks:"))
+num2 = float(input("Enter Physics marks:"))
+num3 = float(input("Enter Chemistry marks:"))
+num4 = float(input("Enter English marks:"))
+num5 = float(input("Enter CS marks:"))
+
+total = num1+num2+num3+num4+num5 
+Percentage = total / 5
+
+if Percentage >= 90:  
+     print("Grade A:")
+elif Percentage >= 85:
+    print("Grade B:")
+elif Percentage >= 75:
+    print("Grade C:")
+elif Percentage >= 65:
+    print("Grade D:")
+
+
